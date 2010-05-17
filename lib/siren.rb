26aaa13d385f90fb2e7b07a0e40616416efb3524
Jeremy Voorhis <jvoorhis@gmail.com>
2010-05-17 08:30:02 -0700

    Moved utility code from test_helper.rb into siren.rb.

--- lib/siren.rb
+++ lib/siren.rb
@@ -6,3 +6,20 @@ require 'siren/code_generator'
 require 'siren/voice'
 require 'siren/dsp_kernel'
 
+module Kernel
+  def target_type_for(tag)
+    case tag
+    when :float then LLVM::Float
+    else
+      fail "Unsupported type #{v.type}"
+    end
+  end
+  module_function :target_type_for    
+end
+
+module Siren
+  def init
+    C.InitDSPSystem
+  end
+  module_function :init
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,17 +1,6 @@
 require 'test/unit'
 require 'siren'
 
-module Kernel
-  def target_type_for(tag)
-    case tag
-    when :float then LLVM::Float
-    else
-      fail "Unsupported type #{v.type}"
-    end
-  end
-  module_function :target_type_for    
-end
-
 class FFI::MemoryPointer
   def read_array_of_float(size)
     read_array_of_type(:float, :read_float, size)
