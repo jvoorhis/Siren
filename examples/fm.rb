83c4ab0f7a7d85277c6784ba9f419a7c9649fe1a
Jeremy Voorhis <jvoorhis@gmail.com>
2010-05-31 12:47:52 -0700

    Refactored FM example

--- examples/fm.rb
+++ examples/fm.rb
@@ -13,22 +13,27 @@ class FM < Siren::Voice
   param :index,    :float, 0.4
 
   def render(gt, vt, c)
-    # stereo tremolo
-    (0.2 * sin((c.to_f + PI) * 2 * PI * 1.5 * gt) + 0.8) * 
-    # operator 1
-    env(0.3 * level, duration, 0.01, 15, 0.2, vt) * sin(2 * PI * freq * vt +
-      # operator 2
-      index * env(1, 3, 0.01, 3, 0.01, vt) * sin(2 * PI * freq * ratio * vt +
-        # operator 3
-        index * env(0.5, 0.1, 0.01, 0.1, 0.01, vt) * sin(2 * PI * 880 * vt)))
+    trem(0.3, 0.79, c, gt,
+      # operator 1
+      env(0.3 * level, duration, 0.01, 15, 0.2, vt) * sin(2 * PI * freq * vt +
+        # operator 2
+        index * env(1, 3, 0.01, 3, 0.01, vt) * sin(2 * PI * freq * ratio * vt +
+          # operator 3
+          index * env(0.5, 0.1, 0.01, 0.1, 0.01, vt) * sin(2 * PI * 880 * vt))))
   end
 
+  # envelope generator
   def env(level, duration, a, d, r, t)
     level * (
       u(t)                * (1 - u(t - a))            * t / a +
       u(t - a)            * (1 - u(t - duration + r)) * (1 - (t - a) / d) +
       u(t - duration + r) * (1 - u(t - duration))     * (1 - (t - a) / d) * (1 - (t - duration + r) / r)
     )
+  end
+
+  # stereo tremolo
+  def trem(mix, freq, chan, phase, sig)
+    sig * (mix * sin((chan.to_f + PI) * 2 * PI * freq * phase) + (1 - mix))
   end
 
   def tail
